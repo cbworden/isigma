1c7e4e87c877239a2f52bbe8ea54102eb8db3577
vinceq-usgs <vinceq@usgs.gov>
2019-11-04 12:10:03 -0800

    Add compareInts, output

--- bootstrapplot.py
+++ bootstrapplot.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as p
 import numpy as np
 
 
-def plot(title, data):
+def plot(title, data, show=False):
 
     fig, ax = p.subplots()
     xbound = 120
@@ -10,6 +10,8 @@ def plot(title, data):
 
     # Plot stddev for each boostrap realization
 
+    bootstrapped_x = None
+    bootstrapped_y = None
     stddevs_by_nresps = {}
     for station in data:
         # id = station['id']
@@ -18,37 +20,52 @@ def plot(title, data):
         stddev = station['stddev']
         # print('%s: n=%i (%i) s=%.2f' % (id, nresps, nresamples, stddev))
 
-        stddevs = station['stddevs']
-        x = np.full(stddevs.shape, fill_value=nresps)
-        ax.scatter(x, stddevs, c='grey', s=5, alpha=0.25)
+        # Save bootstrapped scatter points
+        y = station['stddevs']
+        x = np.full(y.shape, fill_value=nresps)
+        if bootstrapped_x is None:
+            bootstrapped_x = x
+            bootstrapped_y = y
+        else:
+            bootstrapped_x = np.append(bootstrapped_x, x)
+            bootstrapped_y = np.append(bootstrapped_y, y)
+
+        # Save mean of stddev
         if nresps in stddevs_by_nresps:
             stddevs_by_nresps[nresps].append(stddev)
         else:
             stddevs_by_nresps[nresps] = [stddev]
 
-    # Plot mean stddev for each nresp bin
+    # Plot scatterplot
+    ax.scatter(bootstrapped_x, bootstrapped_y, c='grey', s=5, alpha=0.25, label='Bootstrapped STDDEV')
 
+    # Plot mean stddev for each nresp bin
     mean_x = []
     mean_y = []
     for nresps, stddevs in stddevs_by_nresps.items():
         mean_x.append(nresps)
-        mean_stddevs = np.array(stddevs).mean()
+        mean_stddevs = np.nanmean(np.array(stddevs))
         mean_y.append(mean_stddevs)
 
-    ax.scatter(mean_x, mean_y, c='black', s=10)
+    print(mean_x)
+    print(mean_y)
+    ax.scatter(mean_x, mean_y, c='black', s=20, label='Mean STDDEV for each nresp')
 
     # Plot DYFI stddev calculation (WGRW11)
 
     dyfi_x = np.linspace(1, xbound, 500)
     dyfi_y = np.exp(dyfi_x * (-1/24.02)) * 0.25 + 0.09
-    p.plot(dyfi_x, dyfi_y, c='black')
+    ax.plot(dyfi_x, dyfi_y, c='black', label='DYFI computed STDDEV')
 
     ax.set_title(title)
     ax.set_xlabel('Number of responses', fontsize=15)
     ax.set_ylabel('Stddev of intensity', fontsize=15)
     ax.set_xbound(lower=1, upper=xbound)
     ax.set_ybound(lower=0, upper=ybound)
+    ax.legend()
 
     ax.grid(True)
     fig.tight_layout()
-    p.show()
+    if show:
+        p.show()
+    return fig
